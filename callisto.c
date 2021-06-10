493baa3334b79793fd606991cb6d20aa40297241
Mateusz Różański <rozanski.mateusz00@gmail.com>
2021-06-11 00:42:45 +0200

    disable raw mode at exit

--- callisto.c
+++ callisto.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disable_raw_mode();
+void enable_raw_mode();
 
 int main() {
-  printf("Hello, world!\n");
+  enable_raw_mode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
+}
+
+void disable_raw_mode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disable_raw_mode);
+
+  struct termios raw = orig_termios;
+
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
