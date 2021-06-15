9a2173a58245ac6bcdfa891c313ec1a8957447fb
Mateusz Różański <rozanski.mateusz00@gmail.com>
2021-06-16 01:29:19 +0200

    getting terminal window size, fixing vim-like tildes

--- callisto.c
+++ callisto.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -12,7 +13,13 @@
 
 /* Data */
 
-struct termios orig_termios;
+struct editor_config {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editor_config E;
 
 /* Declarations */
 
@@ -23,11 +30,14 @@ char editor_read_key();
 void editor_process_keypress();
 void editor_refresh_screen();
 void editor_draw_rows();
+int get_window_size();
+void init_editor();
 
 /* Init */
 
 int main() {
   enable_raw_mode();
+  init_editor();
 
   while (1) {
     editor_refresh_screen();
@@ -36,10 +46,15 @@ int main() {
   return 0;
 }
 
+void init_editor() {
+  if (get_window_size(&E.screenrows, &E.screencols) == -1)
+    die("get_window_size");
+}
+
 /* Output */
 
 void editor_draw_rows() {
-  for (int i = 0; i < 24; i++) {
+  for (int i = 0; i < E.screenrows; i++) {
     write(STDIN_FILENO, "~\r\n", 3);
   }
 }
@@ -68,6 +83,19 @@ void editor_process_keypress() {
 
 /* Terminal */
 
+int get_window_size(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  }
+  else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 char editor_read_key() {
   int nread;
   char c;
@@ -87,20 +115,20 @@ void die(const char *s) {
 }
 
 void disable_raw_mode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
     die("tcsetattr");
   }
 }
 
 void enable_raw_mode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
     die("tcgetattr");
   }
 /* we use it to call disable_raw_mode() automatically when program exits,
  * either by returning from main(), or by calling the exit() function */
   atexit(disable_raw_mode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
 
   tcgetattr(STDIN_FILENO, &raw);
   /* The ECHO feature causes each key you type to be printed to the terminal */
