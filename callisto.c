18954d48666ed90eadb3d8402d96a972a7263c2b
Mateusz Różański <rozanski.mateusz00@gmail.com>
2021-06-16 01:55:32 +0200

    getting window size for all terminals, hard way

--- callisto.c
+++ callisto.c
@@ -31,6 +31,7 @@ void editor_process_keypress();
 void editor_refresh_screen();
 void editor_draw_rows();
 int get_window_size();
+int get_cursor_position(int *, int *);
 void init_editor();
 
 /* Init */
@@ -83,11 +84,37 @@ void editor_process_keypress() {
 
 /* Terminal */
 
+int get_cursor_position(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+
+  return 0;
+}
+
 int get_window_size(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      return -1;
+    return get_cursor_position(rows, cols);
   }
   else {
     *cols = ws.ws_col;
