d2f75167c3d7a5ff5871c2aefe97b9159d207d3d
Mateusz Różański <rozanski.mateusz00@gmail.com>
2021-07-06 17:22:57 +0200

    limit scrolling to right

--- callisto.c
+++ callisto.c
@@ -216,6 +216,7 @@ void editor_refresh_screen() {
 /* Input */
 
 void editor_move_cursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -223,7 +224,9 @@ void editor_move_cursor(int key) {
       }
       break;
     case ARROW_RIGHT:
+      if (row && E.cx < row->size) {
         E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
