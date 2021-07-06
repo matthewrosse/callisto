b15ab83df4dd05a3f6d136fc4e157d9701249434
Mateusz Różański <rozanski.mateusz00@gmail.com>
2021-07-06 17:20:40 +0200

    horizontal scrolling

--- callisto.c
+++ callisto.c
@@ -40,6 +40,7 @@ typedef struct erow {
 struct editor_config {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -96,6 +97,7 @@ void init_editor() {
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
   if (get_window_size(&E.screenrows, &E.screencols) == -1)
@@ -143,6 +145,12 @@ void editor_scroll() {
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 void editor_draw_rows(struct abuf *ab) {
@@ -172,10 +180,10 @@ void editor_draw_rows(struct abuf *ab) {
       }
     }
     else {
-      int len = E.row[filerow].size;
-      if (len > E.screencols)
-        len = E.screencols;
-      ab_append(ab, E.row[filerow].chars, len);
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0)
+        len = 0;
+      ab_append(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
     ab_append(ab, "\x1b[K", 3);
@@ -196,7 +204,7 @@ void editor_refresh_screen() {
   editor_draw_rows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
   ab_append(&ab, buf, strlen(buf));
 
   ab_append(&ab, "\x1b[?25h", 6);
@@ -215,9 +223,7 @@ void editor_move_cursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols - 1) {
         E.cx++;
-      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
